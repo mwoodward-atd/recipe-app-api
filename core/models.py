90ef20a974f4400777483cc652ecc662b3ecea08
Matthew Woodward <matthew_woodward@saa.senate.gov>
2020-01-24 22:42:46 +0000

    added working list tags endpoint

--- core/models.py
+++ core/models.py
@@ -41,7 +41,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Tag(models.Model):
     """Tag to be used for a recipe"""
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
