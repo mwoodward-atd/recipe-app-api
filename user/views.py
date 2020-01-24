76623b673ef95f0eb7e7325306a0dd095bf776d4
Matthew Woodward <matthew_woodward@saa.senate.gov>
2020-01-24 19:18:53 +0000

    added manage user endpoint

--- user/views.py
+++ user/views.py
@@ -1,13 +1,24 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.generics import CreateAPIView
+from rest_framework import generics, authentication, permissions
 from rest_framework.settings import api_settings
 
 from .serializers import UserSerializer, AuthTokenSerializer
 
 
-class CreateUserView(CreateAPIView):
+class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication, )
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
 
 
 class CreateTokenView(ObtainAuthToken):
